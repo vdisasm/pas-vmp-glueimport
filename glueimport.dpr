259ca428d89367f8836d226bb8ce3130cf271957
vdisasm <vdisasmdev@gmail.com>
2016-04-19 16:33:56 +0300

    Initial files

---
+++ glueimport.dpr
@@ -0,0 +1,171 @@
+program glueimport;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+{$WARN SYMBOL_PLATFORM OFF}
+
+
+uses
+  System.Classes,
+  System.Generics.Collections,
+  System.SysUtils,
+
+  vmsweeper.reffile in 'vmsweeper.reffile.pas',
+
+  pe.build,
+
+  pe.build.import,
+  pe.common,
+  pe.image,
+  pe.imports,
+  pe.imports.lib,
+  pe.imports.func,
+  pe.section,
+  pe.types.directories;
+
+procedure generate_new_imports(img: TPEImage; Libs: TLibs);
+const
+  opcJmpPtr: word = $25FF;
+var
+  libPair: TLibPair;
+  funcPair: TFuncPair;
+  tmpVA, iatVA: uint64;
+  lib: TPEImportLibrary;
+  iatIndex: UInt32;
+  imps: TImpList;
+  i: Integer;
+  rec: TImp;
+  FlatImpList: TImpList;
+  iatDir: TImageDataDirectory;
+begin
+
+  FlatImpList := TImpList.Create;
+  try
+    // Fill image imports.
+
+    img.imports.Clear;
+
+    iatIndex := 0;
+    for libPair in Libs do
+    begin
+      writeln('  ', libPair.Key);
+
+      lib := img.imports.NewLib(libPair.Key);
+      for funcPair in libPair.Value do
+      begin
+        lib.NewFunction(funcPair.Key);
+
+        imps := funcPair.Value;
+        for i := 0 to imps.Count - 1 do
+        begin
+          rec := imps[i];
+          rec.iatIndex := iatIndex;
+          FlatImpList.Add(rec);
+        end;
+
+        inc(iatIndex);
+      end;
+
+      inc(iatIndex); // null item
+    end;
+
+    // Create new import table.
+    ReBuildDirData(img, DDIR_IMPORT, true);
+
+    // Redirect imports to new IAT.
+    if not img.DataDirectories.Get(DDIR_IAT, @iatDir) then
+      raise Exception.Create('Failed to get IAT.');
+
+    iatVA := img.RVAToVA(iatDir.VirtualAddress);
+
+    for rec in FlatImpList do
+    begin
+      // jmp dword ptr [...]
+      img.PositionVA := rec.srcVA;
+      img.Write(opcJmpPtr, sizeof(opcJmpPtr));
+
+      // address
+      tmpVA := iatVA + rec.iatIndex * img.ImageWordSize;
+      img.Write(tmpVA, img.ImageWordSize)
+    end;
+  finally
+    FreeAndNil(FlatImpList);
+  end;
+end;
+
+procedure PrintHeader;
+begin
+  writeln('VMProtect import fixer based on VMSweeper');
+  writeln('Credits to Vamit');
+  writeln;
+  writeln('This tool applies imports found by VMSweeper to existing dump');
+  writeln('Generally it''s for analysis, not for complete image unpack');
+  writeln;
+  writeln('Usage:');
+  writeln('  <ref> <dump> [<out>]');
+  writeln('  <ref>:  text file, just copy it from VMSweeper''s VM References window');
+  writeln('          it must contain resolved imports');
+  writeln('  <dump>: executable dump file name used to add imports');
+  writeln('  <out>:  optional file name for result executable');
+end;
+
+procedure main(const refFn, dumpFn, outFn: string);
+var
+  Libs: TLibs;
+  img: TPEImage;
+  outputFn: string;
+begin
+  if (refFn.IsEmpty) or (dumpFn.IsEmpty) then
+  begin
+    PrintHeader;
+    exit;
+  end;
+
+  Libs := TLibs.Create([doOwnsValues]);
+  try
+    write('Parsing reference file... ');
+    ParseImportsFromRefFile(refFn, Libs);
+    writeln(Libs.Count, ' libraries');
+
+    img := TPEImage.Create;
+    try
+      write('Reading dump... ');
+      // load dump only, don't parse directories' content
+      if not img.LoadFromFile(dumpFn, []) then
+      begin
+        writeln('failed');
+        exit;
+      end;
+      writeln('ok');
+
+      writeln('Creating new imports');
+      generate_new_imports(img, Libs);
+
+      if outFn <> '' then
+        outputFn := outFn
+      else
+        outputFn := ChangeFileExt(dumpFn, '_imp' + ExtractFileExt(dumpFn));
+
+      writeln('Saving new image');
+      img.SaveToFile(outputFn);
+
+      writeln('Done');
+    finally
+      img.Free;
+    end;
+  finally
+    FreeAndNil(Libs);
+  end;
+end;
+
+begin
+  ReportMemoryLeaksOnShutdown := true;
+  try
+    main(paramstr(1), paramstr(2), paramstr(3));
+    if DebugHook <> 0 then
+      readln;
+  except
+    on E: Exception do
+      writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
---
+++ vmsweeper.reffile.pas
@@ -0,0 +1,137 @@
+unit vmsweeper.reffile;
+
+interface
+
+uses
+  System.Classes,
+  System.Generics.Collections,
+  System.SysUtils;
+
+type
+  // List of addresses referencing same function.
+  TImp = record
+    srcVA: uint64;
+    iatIndex: uint32;
+  end;
+
+  TImpList = TList<TImp>;
+
+  TFuncName = string;
+  TFuncPair = TPair<TFuncName, TImpList>;
+  TFuncs = TObjectDictionary<TFuncName, TImpList>;
+
+  TLibName = string;
+  TLibPair = TPair<TLibName, TFuncs>;
+  TLibs = TObjectDictionary<TLibName, TFuncs>;
+
+procedure ParseImportsFromRefFile(const RefFileName: string; Libs: TLibs);
+
+implementation
+
+const
+  HexDigits = ['0' .. '9', 'a' .. 'f', 'A' .. 'F'];
+
+function IsHex(const s: string): boolean;
+var
+  c: char;
+begin
+  if s.IsEmpty then
+    exit(false);
+  for c in s do
+    if not(CharInSet(c, HexDigits)) then
+      exit(false);
+  exit(true);
+end;
+
+procedure SplitWords(const line: string; words: TStringList);
+var
+  word: string;
+  p: pchar;
+  i, w0: integer;
+begin
+  words.Clear;
+  if line.IsEmpty then
+    exit;
+  p := pchar(line);
+  i := 0;
+  w0 := -1;
+  while i <= length(line) do
+  begin
+    if (p[0] = #0) or ((p[0] = ' ') and (p[1] = ' ')) then
+    begin
+      if w0 <> -1 then
+      begin
+        word := line.Substring(w0, i - w0);
+        words.Add(word);
+        w0 := -1;
+      end;
+    end
+    else if (p[0] <> ' ') then
+    begin
+      if w0 = -1 then
+        w0 := i;
+    end;
+
+    inc(p);
+    inc(i);
+  end;
+end;
+
+procedure AddFunc(va: uint64; const lib, func: string; Libs: TLibs);
+var
+  libName: string;
+  funcs: TFuncs;
+  list: TImpList;
+  rec: TImp;
+begin
+  libName := lib.ToLower;
+  if not Libs.TryGetValue(libName, funcs) then
+  begin
+    funcs := TFuncs.Create([doOwnsValues]);
+    Libs.Add(libName, funcs);
+  end;
+
+  if not funcs.TryGetValue(func, list) then
+  begin
+    list := TImpList.Create;
+    funcs.Add(func, list);
+  end;
+
+  rec.srcVA := va;
+  rec.iatIndex := 0;
+
+  list.Add(rec);
+end;
+
+procedure ParseImportsFromRefFile(const RefFileName: string; Libs: TLibs);
+var
+  sl: TStringList;
+  i: integer;
+  words: TStringList;
+  libFunc: TArray<string>;
+  va: uint64;
+begin
+  sl := TStringList.Create;
+  words := TStringList.Create;
+  try
+    sl.LoadFromFile(RefFileName);
+    for i := 0 to sl.Count - 1 do
+    begin
+      SplitWords(sl[i], words);
+      if (words.Count = 4) and words[3].Equals('Done') and IsHex(words[0]) then
+      begin
+        libFunc := words[2].Split(['.']);
+        if (length(libFunc) = 2) then
+        begin
+          va := uint64(StrToInt64('$' + words[0]));
+          AddFunc(va, libFunc[0], libFunc[1], Libs);
+        end;
+      end;
+    end;
+  finally
+    sl.Free;
+    words.Free;
+  end;
+end;
+
+end.
